78c1480fa36d09f378c6b857050c2b9eb6732c3f
letspks <96194211+letspks@users.noreply.github.com>
2022-02-17 21:10:16 +0100

    kalkulator z dzieleniem

--- main.c
+++ main.c
@@ -8,10 +8,10 @@ int a, b, wynik, dzialanie;
 printf("Kalkulator mnozacy, dodajacy, odejmujacy\n");
 printf("Wprowadz liczbe a:\n");
 scanf("%d", &a);
+printf("Jesli chcesz pomnozyc liczbe a z b wybierz 1,\n jezeli chcesz odjac liczbe b od liczby a wybierz 2,\n jesli chcesz dodac liczbe b do liczby a wybierz 3,\n jezeli chcesz podzielic a przez b wybierz 4.\n ");
+scanf("%d", &dzialanie);
 printf("Wprowadz liczbe b:\n");
 scanf("%d", &b);
-printf("Jesli chcesz pomnozyc liczbe a z b wybierz 1,\n jezeli chcesz odjac liczbe b od liczby a wybierz 2,\n jesli chcesz dodac liczbe b do liczby a wybierz 3. ");
-scanf("%d", &dzialanie);
 if(dzialanie==1)
 {
     wynik=a*b;
@@ -23,6 +23,17 @@ else if(dzialanie==2)
 else if(dzialanie==3)
 {
     wynik=(a+b);
+}
+else if(dzialanie==4)
+{
+    if(b==0)
+    {
+        printf("Nie wolno dzielic przez zero!!!");
+    }
+    else
+    {
+        wynik=(a/b);
+    }
 }
 printf("%d", wynik);
 }
