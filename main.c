b01c5b497f03a69b9fa7a9ccef7df1b95e892ddd
letspks <96194211+letspks@users.noreply.github.com>
2022-02-21 22:41:50 +0100

    zmieniono na polecenie 4 i switche

--- main.c
+++ main.c
@@ -4,25 +4,52 @@
 int main()
 
 {
-int a, b, wynik, dzialanie;
-printf("Kalkulator mnozacy, dodajacy, odejmujacy\n");
-printf("Wprowadz liczbe a:\n");
-scanf("%d", &a);
-printf("Wprowadz liczbe b:\n");
-scanf("%d", &b);
-printf("Jesli chcesz pomnozyc liczbe a z b wybierz 1,\n jezeli chcesz odjac liczbe b od liczby a wybierz 2,\n jesli chcesz dodac liczbe b do liczby a wybierz 3. ");
-scanf("%d", &dzialanie);
-if(dzialanie==1)
+float b=0, wynik=0;
+int operacja=9;
+printf("Kalkulator mnozacy, dodajacy, odejmujacy, dzielacy\n");
+printf("Dostepne operacje:\n");
+printf("0.Podaj wynik\n");
+printf("1.Dodawanie\n");
+printf("2.Odejmowanie\n");
+printf("3.Mnozenie\n");
+printf("4.Dzielenie\n");
+printf("Wprowadz liczbe poczatkowa: ");
+scanf("%f", &wynik);
+while(operacja!=0)
 {
-    wynik=a*b;
+    printf("Wybierz operacje (0, 1, 2, 3, 4): ");
+    scanf("%d", &operacja);
+    if (operacja == 0)
+    {
+        printf("Wynik to: %f", wynik);
+        exit(0);
+    }
+    printf("Wpisz druga liczbe: ");
+    scanf("%f", &b);
+        switch (operacja)
+        {
+        case 1:
+            wynik=b+wynik;
+            break;
+        case 2:
+            wynik=wynik-b;
+            break;
+        case 3:
+            wynik=wynik*b;
+            break;
+        case 4:
+            if(b==0)
+            {
+                printf("UWAGA, DZIELENIE PRZEZ 0!!!\n Wybierz ponownie operacje\n");
+                break;
+            }
+            else
+            {
+                wynik=wynik/b;
+                break;
+            }
+        default:
+            printf("Nie rozpoznano operacji.\n");
+        }
 }
-else if(dzialanie==2)
-{
-    wynik=(a-b);
-}
-else if(dzialanie==3)
-{
-    wynik=(a+b);
-}
-printf("%d", wynik);
 }
