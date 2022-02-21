d94be3f180e8f1e38a3ef4483b280c2eff9be1d9
letspks <96194211+letspks@users.noreply.github.com>
2022-02-21 23:10:37 +0100

    silnia

--- main.c
+++ main.c
@@ -1,28 +1,23 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+unsigned long long silnia(int x)
+{
+    if(x==1)
+    {
+        return 1;
+    }
+    else
+    {
+        return x*silnia(x-1);
+    }
+
+}
 
 int main()
-
 {
-int a, b, wynik, dzialanie;
-printf("Kalkulator mnozacy, dodajacy, odejmujacy\n");
-printf("Wprowadz liczbe a:\n");
-scanf("%d", &a);
-printf("Wprowadz liczbe b:\n");
-scanf("%d", &b);
-printf("Jesli chcesz pomnozyc liczbe a z b wybierz 1,\n jezeli chcesz odjac liczbe b od liczby a wybierz 2,\n jesli chcesz dodac liczbe b do liczby a wybierz 3. ");
-scanf("%d", &dzialanie);
-if(dzialanie==1)
-{
-    wynik=a*b;
-}
-else if(dzialanie==2)
-{
-    wynik=(a-b);
-}
-else if(dzialanie==3)
-{
-    wynik=(a+b);
-}
-printf("%d", wynik);
+    int x;
+    printf("Podaj x (liczbe na ktorej trzeba przeprowadzic operacje silni): ");
+    scanf("%d", &x);
+    printf("Wynik: %llu\n", silnia(x));
 }
